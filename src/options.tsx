32754caf3cc7761e8540efa23c2968bb02eec14b
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-05-08 17:28:29 +0900

    🛠create setting page

--- src/options.tsx
+++ src/options.tsx
@@ -2,21 +2,18 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Options = () => {
-  const [color, setColor] = useState<string>("");
+  const [name, setName] = useState<string>("");
   const [status, setStatus] = useState<string>("");
-  const [like, setLike] = useState<boolean>(false);
 
   useEffect(() => {
     // Restores select box and checkbox state using the preferences
     // stored in chrome.storage.
     chrome.storage.sync.get(
       {
-        favoriteColor: "red",
-        likesColor: true,
+        favoriteColor: "hoge",
       },
       (items) => {
-        setColor(items.favoriteColor);
-        setLike(items.likesColor);
+        setName(items.projectName);
       }
     );
   }, []);
@@ -25,8 +22,7 @@ const Options = () => {
     // Saves options to chrome.storage.sync.
     chrome.storage.sync.set(
       {
-        favoriteColor: color,
-        likesColor: like,
+        projectName : name
       },
       () => {
         // Update status to let user know options were saved.
@@ -42,25 +38,12 @@ const Options = () => {
   return (
     <>
       <div>
-        Favorite color: <select
-          value={color}
-          onChange={(event) => setColor(event.target.value)}
-        >
-          <option value="red">red</option>
-          <option value="green">green</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={like}
-            onChange={(event) => setLike(event.target.checked)}
-          />
-          I like colors.
-        </label>
+      プロジェクトのURL: https://scrapbox.io/:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </div>
       <div>{status}</div>
       <button onClick={saveOptions}>Save</button>
